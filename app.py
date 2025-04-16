0fe1d1bb2c360fcf931259185b513eca4bceba06
Ayelen Quispe <quispeaguilar.bel3n@gmail.com>
2025-04-16 11:56:53 -0300

    Hice la conexion a base de datos

--- app.py
+++ app.py
@@ -1,17 +1,43 @@
 from flask import Flask, url_for
+import sqlite3
 
 app = Flask(__name__)
+#------CONEXION A BASE DE DATOS
+db=None
+def abrirConexion():
+    global db
+    db = sqlite3.connect("instance/datos.sqlite")
+    db.row_factory = sqlite3.Row
+    return db
+
+def cerrarConexion():
+    global db
+    if db is not None:
+        db.close()
+        db = None
+
+@app.route("/usuarios/")
+def obterGente():
+    global db
+    conexion = abrirConexion()
+    cursor = conexion.cursor()
+    cursor.execute('SELECT * FROM usuarios')
+    resultado = cursor.fetchall()# fetchall te selecciona todos / fetchone te trae un solo resultado
+    cerrarConexion()
+    fila = [dict(row) for row in resultado]# fila por fila
+    return str(fila)# te lo devuelve en string
+#------
 
 @app.route("/")
 def principal():
-    url_hola = url_for("consultaxn")
+    url_hola = url_for("consultaxn", nombre='Aye')
     url_dado = url_for("dado", caras=6)
     url_logo = url_for("static", filename="aquarius.jpg")
 
     return f"""
-    <a href="{url_hola}">Hola</a>
+    <a href="{url_hola}">Consulta</a>
     <br>
-    <a href="{url_for("bye")}">Chau</a>
+    <a href="{url_for("respuesta")}">Respuesta</a>
     <br>
     <a href="{url_logo}">Logo</a>
     <br>
@@ -42,7 +68,7 @@ def suma(n1,n2):
 def respuesta():
     return "<p>Estoy bien</p>"
 
-#mi ejemplo
+#------ Mi ejemplo
 @app.route("/consultaxn/<string:nombre>")
 def consultaxn(nombre):
     return f"<p>¿Cómo estas {nombre}?</p>"
